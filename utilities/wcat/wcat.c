9657e56b45be3d7c3d22aa4d98ffa009986ad7ed
dbarrett <longhornbarrett@gmail.com>
2022-05-21 19:18:28 -0700

    Initial wcat program

--- utilities/wcat/wcat.c
+++ utilities/wcat/wcat.c
@@ -1,1 +1,20 @@
-//Hey your code goes here..!
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    int BUF_SIZE = 1024;
+    char str[BUF_SIZE];
+    FILE *fh;
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            fh = fopen(argv[i], "r");
+            if (fh == NULL) {
+                printf("wcat: cannot open file\n");
+                return 1;
+            }
+            while (fgets(str, BUF_SIZE, fh) != NULL)
+                fputs(str, stdout);
+            fclose(fh);
+        }
+    }
+    return 0;
+}
